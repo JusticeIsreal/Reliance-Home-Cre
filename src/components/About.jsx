e1fc4a98bd16376d2cbfae20d7acac46a291b657
Justice Isreal <justiceyba@gmail.com>
2024-01-01 18:54:53 -0500

    Services setup

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,18 +3,22 @@ import img from "../assets/img/about.jpg";
 
 const About = () => {
   return (
-    <div className="  flex flex-col lg:flex-row justify-between items-center lg:px-15 xl:px-20 px-5 pt-20 lg:mt-20 gap-5">
+    <div className="  flex flex-col lg:flex-row justify-between items-center lg:px-15 xl:px-20 px-5 pt-10 lg:mt-20 gap-5">
       <div className=" w-full lg:w-3/4 space-y-4">
         <h1 className=" text-4xl font-semibold text-start text-[#9a1aca]">
           About Us
         </h1>
         <p className="lg:text-start">
           <span className="text-[#9a1aca]">
-            Reliance Home Health Caregivers
+            Reliance Home Health Caregivers{" "}
           </span>
           is a home health care agency that is independently owned and operated.
-          We are located in 2625 Butterfield Road, Suite 314E, Oakbrook,
-          Illinois 60523. You can click here to take a look at our map.
+          We do not discriminate anyone on the basis of gender, color, race,
+          origin, ethnicity, nationality or creed. Whoever you are and as long
+          as you are qualified and in need of our services, our doors are always
+          open for you. We are located in 2625 Butterfield Road, Suite 314E,
+          Oakbrook, Illinois 60523. You can click here to take a look at our
+          map.
         </p>
         <p className="text-justify lg:text-start">
           This mission is set on motion by continually providing our services in
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,37 +6,31 @@ import { MdHealthAndSafety } from "react-icons/md";
 import { FaHeartbeat } from "react-icons/fa";
 
 const Services = () => {
-  const icon1 = (
-    <RiMicroscopeLine size={35} className=" text-backgroundColor" />
-  );
-  const icon2 = (
-    <MdHealthAndSafety size={35} className=" text-backgroundColor" />
-  );
-  const icon3 = <FaHeartbeat size={35} className=" text-backgroundColor" />;
+  const icon1 = <RiMicroscopeLine size={35} className=" text-[#9a1aca]" />;
+  const icon2 = <MdHealthAndSafety size={35} className=" text-[#9a1aca]" />;
+  const icon3 = <FaHeartbeat size={35} className=" text-[#9a1aca]" />;
 
   return (
-    <div className=" min-h-screen flex flex-col justify-center lg:px-30 xl:px-30 px-5 pt-24 lg:pt-16">
+    <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 pt-24 lg:pt-16">
       <div className=" flex flex-col items-center lg:flex-row justify-between">
-        <div>
-          <h1 className=" text-4xl font-semibold text-center lg:text-start">
+        <div className="m-auto">
+          <h1 className=" text-4xl font-semibold text-center  text-[#9a1aca]">
             Our Services
           </h1>
-          <p className=" mt-2 text-center lg:text-start">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus,
-            quidem.
+          <p className=" mt-2 text-center " style={{ maxWidth: "700px" }}>
+            We bring forth good news by being the agency that you can truly rely
+            on. We bring you care at home. Are you concerned about the Safety &
+            Care of an aging or sick parent/child? WE CAN HELP!
           </p>
-        </div>
-        <div className=" mt-4 lg:mt-0">
-          <Button title="See Services" />
         </div>
       </div>
       <div className=" flex flex-col md:flex-row flex-wrap justify-evenly gap-5 pt-20">
-        <ServicesCard icon={icon1} title="Lab Test" />
-        <ServicesCard icon={icon2} title="Health Check" />
-        <ServicesCard icon={icon3} title="Heart Health" />
-        <ServicesCard icon={icon1} title="Lab Test" />
-        <ServicesCard icon={icon2} title="Health Check" />
-        <ServicesCard icon={icon3} title="Heart Health" />
+        <ServicesCard icon={icon1} title="Home Nursing Services" />
+        <ServicesCard icon={icon2} title="24 Hour In Home Care" />
+        <ServicesCard icon={icon3} title="Speech Therapy Services" />
+        <ServicesCard icon={icon1} title="Home Health Services" />
+        <ServicesCard icon={icon2} title="Home Health Aid" />
+        <ServicesCard icon={icon3} title="Therapy Health Services" />
       </div>
     </div>
   );
